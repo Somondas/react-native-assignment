09ac367bc9ab3225e30ca827df6cede45207806c
Somondas <somondas067@gmail.com>
2024-08-13 11:14:58 +0530

    Updates

--- screens/Home.js
+++ screens/Home.js
@@ -1,13 +1,25 @@
-import { Button, FlatList, Image, StyleSheet, Text, View } from "react-native";
+import {
+  Button,
+  FlatList,
+  Image,
+  Modal,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
 import React, { Component, useEffect, useState } from "react";
 import Heading from "../components/Heading";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { Icon } from "react-native-elements";
 
 const Home = () => {
   const [products, setProducts] = useState([]);
   const [loading, setLoading] = useState(true);
-  const [showMore, setShowMore] = useState(10);
+  const [modalVisible, setModalVisible] = useState(false);
+  const [selectedProduct, setSelectedProduct] = useState(null);
 
+  // >> Fetch Product Details__________________________
   useEffect(() => {
     const fetchProducts = async () => {
       try {
@@ -28,6 +40,19 @@ const Home = () => {
 
     fetchProducts();
   }, []);
+
+  // -> Modal Functions_____________________________
+  const openModal = (product) => {
+    setSelectedProduct(product);
+    setModalVisible(true);
+  };
+
+  const closeModal = () => {
+    setModalVisible(false);
+    setSelectedProduct(null);
+  };
+
+  // >> Pre-Loader___________________________________________
   if (loading) {
     return (
       <View style={styles.loadingContainer}>
@@ -35,6 +60,7 @@ const Home = () => {
       </View>
     );
   }
+
   return (
     <SafeAreaView style={{ flex: 1, paddingTop: 60 }}>
       <Heading />
@@ -53,18 +79,57 @@ const Home = () => {
                 <Text style={styles.productPrice}>
                   Rs.{item.price.toFixed(2)}
                 </Text>
-                <View style={styles.productButton}>
-                  <Button title="Details" />
-                </View>
+                <TouchableOpacity onPress={() => openModal(item)}>
+                  <Text style={styles.detailsBtn}>Details</Text>
+                </TouchableOpacity>
               </View>
             </View>
           )}
         />
+        <Modal
+          animationType="slide"
+          transparent={true}
+          visible={modalVisible}
+          onRequestClose={closeModal}
+        >
+          {selectedProduct && (
+            <View style={styles.modalContainer}>
+              <View style={styles.modalContent}>
+                <TouchableOpacity
+                  onPress={closeModal}
+                  style={styles.closeButton}
+                >
+                  <Text>
+                    <Icon name="close" color={"#f23712"} />
+                  </Text>
+                </TouchableOpacity>
+                <Image
+                  source={{ uri: selectedProduct.images[0] }}
+                  style={{
+                    width: "100%",
+                    height: 200,
+                    marginTop: 2,
+                  }}
+                />
+                <Text style={styles.modalTitle}>{selectedProduct.title}</Text>
+                <Text style={styles.modalPrice}>
+                  Rs.{selectedProduct.price.toFixed(2)}
+                </Text>
+                <Text style={styles.modalDescription}>
+                  {selectedProduct.description.slice(0, 100)}...
+                </Text>
+                <TouchableOpacity style={styles.addToCartButton}>
+                  <Text style={styles.addToCartButtonText}>Add To Cart</Text>
+                </TouchableOpacity>
+              </View>
+            </View>
+          )}
+        </Modal>
       </View>
     </SafeAreaView>
   );
 };
-
+// >> StyleSheet____________________________________________
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -108,6 +173,82 @@ const styles = StyleSheet.create({
   productPrice: {
     fontSize: 14,
     color: "#888",
+  },
+  detailsBtn: {
+    marginTop: 5,
+    fontSize: 18,
+    backgroundColor: "#2196F3",
+    // width: "",
+    color: "#ffff",
+    paddingHorizontal: 15,
+    paddingVertical: 5,
+    textAlign: "center",
+    width: 110,
+    borderRadius: 3,
+  },
+  // ? Modal Styles__________________
+  modalContainer: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "rgba(255,255,255, .8)",
+    // padding: 20,
+    width: "100%",
+    height: "100%",
+  },
+  modalContent: {
+    backgroundColor: "#fff",
+    padding: 8,
+    width: "90%",
+    height: 410,
+    marginLeft: "auto",
+    marginRight: "auto",
+    borderRadius: 10,
+    borderWidth: 1,
+    borderColor: "black",
+  },
+  modalDescription: {
+    fontSize: 16,
+    textAlign: "left",
+    marginBottom: 10,
+  },
+  modalPrice: {
+    fontSize: 18,
+    color: "#888",
+    marginBottom: 5,
+  },
+  modalTitle: {
+    fontSize: 18,
+    fontWeight: "bold",
+    marginBottom: 5,
+  },
+  addToCartButton: {
+    width: "50%",
+    borderWidth: 3,
+    borderColor: "#2196F3",
+    borderRadius: 5,
+    padding: 5,
+  },
+  addToCartButtonText: {
+    textAlign: "center",
+    fontSize: 18,
+    color: "#2196f3",
+    fontWeight: "bold",
+  },
+  closeButton: {
+    position: "absolute",
+    top: 4,
+    right: 4,
+    zIndex: 1,
+    backgroundColor: "#FFF",
+    borderRadius: 19,
+    borderWidth: 2,
+    borderColor: "#f23712",
+    padding: 5,
+  },
+  closeButtonText: {
+    color: "#fff",
+    fontWeight: "bold",
   },
 });
 export default Home;
